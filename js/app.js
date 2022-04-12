44c60c4caa43839ca8dba9ba54f5c4ac910cb4bd
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2022-04-12 21:33:16 +0500

    main section

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".type", {
+    strings: [
+        'Asadbek', 
+        'Muxtorov',
+        'Frontend Developer',
+        'Freelancer'
+    ],
+    typeSpeed:60,
+    backSpeed:60,
+    loop:true,
+
+  });
